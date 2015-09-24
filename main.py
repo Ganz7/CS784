5cc538016000510d747286e34857caf1ac9b0686
Ganz7 <ganzse7en@gmail.com>
2015-09-24 16:17:06 -0500

    Extracts movie titles from Wiki Page

--- main.py
+++ main.py
@@ -9,7 +9,22 @@ def get_movie_pages(link):
 	page_content = get_html_page(link)
 	soup = BeautifulSoup(page_content, 'html.parser')
 
-	print soup.title
+	#tables = soup.findAll("table", class_="wikitable sortable")
+	#table = tables[0]
+	table = soup.find("table", class_="wikitable sortable")
+	rows = table.findAll("tr")
+	titles = []
+
+	for row in rows:
+		title_row = row.find("td")	
+		if(title_row):
+			title_tag = title_row.find("i").find("a")
+			if(title_tag):
+				titles.append(title_tag.text)
+
+	for title in titles:
+		print title
+
 
 def main():
 	link = "https://en.wikipedia.org/wiki/List_of_American_films_of_2012"
