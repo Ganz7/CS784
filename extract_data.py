17c4fcf6db8512afc2af2363d93c17e8bf08ee96
Ganz7 <ganzse7en@gmail.com>
2015-10-02 18:31:48 -0500

    Extract data functionality initialized

---
+++ extract_data.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+def extract_imdb_data():
+	print "Extracting IMDB Movies data..."
+
+def extract_rottentomatoes_data():
+	print "Extracting Rotten Tomatoes Movies data..."
+
+def main():
+	extract_imdb_data()
+	extract_rottentomatoes_data()
+if __name__ == '__main__':
+	main()
